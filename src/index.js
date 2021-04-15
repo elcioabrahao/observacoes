7953e9702a6ebf4e7d8a631b041a524913451cea
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-04-15 13:31:35 -0300

    Atualizações

--- src/index.js
+++ src/index.js
@@ -8,6 +8,33 @@ const {
     v4: uuidv4
 } = require('uuid');
 
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes =
+            observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id ===
+            observacao.id)
+        obsParaAtualizar.status = observacao.status;
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+app.post("/eventos", (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (e) {}
+    res.status(200).send({
+        msg: "ok"
+    });
+});
+
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
 app.put('/lembretes/:id/observacoes', async (req, res) => {
@@ -20,7 +47,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         observacoesPorLembreteId[req.params.id] || [];
     observacoesDoLembrete.push({
         id: idObs,
-        texto
+        texto,
+        status: 'aguardando'
     });
     observacoesPorLembreteId[req.params.id] =
         observacoesDoLembrete;
@@ -29,7 +57,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         dados: {
             id: idObs,
             texto,
-            lembreteId: req.params.id
+            lembreteId: req.params.id,
+            status: 'aguardando'
         }
     })
     res.status(201).send(observacoesDoLembrete);
